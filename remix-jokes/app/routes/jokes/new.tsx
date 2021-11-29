c645175845f3928e6deb6a97e157572648c87791
wuhuijie <wuhuijie@yiche.com>
2021-11-29 15:49:24 +0800

    feat(Jokes App): 新增joke
    action提交

--- remix-jokes/app/routes/jokes/new.tsx
+++ remix-jokes/app/routes/jokes/new.tsx
@@ -1,3 +1,21 @@
+import type { ActionFunction } from 'remix';
+import { redirect } from 'remix';
+import { db } from '~/utils/db.server';
+
+export let action: ActionFunction = async ({ request }) => {
+  let form = await request.formData();
+  let name = form.get('name');
+  let content = form.get('content');
+
+  if (typeof name !== 'string' || typeof content !== 'string') {
+    throw new Error(`Form not submitted correctly`);
+  }
+
+  let fields = { name, content };
+  let joke = await db.joke.create({ data: fields });
+  return redirect(`/jokes/${joke.id}`);
+};
+
 export default function NewJokeRoute() {
   return (
     <div>
@@ -5,7 +23,7 @@ export default function NewJokeRoute() {
       <form method="post">
         <div>
           <label>
-            name: <input type="text" name="name"/>
+            name: <input type="text" name="name" />
           </label>
         </div>
         <div>
@@ -14,7 +32,9 @@ export default function NewJokeRoute() {
           </label>
         </div>
         <div>
-          <button type="submit" className="button">Add</button>
+          <button type="submit" className="button">
+            Add
+          </button>
         </div>
       </form>
     </div>
