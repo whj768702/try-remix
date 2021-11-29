0ba8379aa5281f312127a14824bfaf3b87525bcc
wuhuijie <wuhuijie@yiche.com>
2021-11-29 14:33:41 +0800

    feat(Jokes App): 增加css

--- remix-jokes/app/root.tsx
+++ remix-jokes/app/root.tsx
@@ -1,15 +1,27 @@
-import { LiveReload, Outlet } from "remix";
+import { LiveReload, Outlet, LinksFunction, Links } from 'remix';
+import globalStyleUrl from './styles/global.css';
+import globalMediumStyleUrl from './styles/global-medium.css';
+import globalLargeStyleUrl from './styles/global-large.css';
+
+export let links: LinksFunction = () => {
+  return [
+    { rel: 'stylesheet', href: globalStyleUrl },
+    { rel: 'stylesheet', href: globalMediumStyleUrl, media: 'print, (min-width:640px)' },
+    { rel: 'stylesheet', href: globalLargeStyleUrl, media: 'screen and (min-width: 1024px)' },
+  ];
+};
 
 export default function App() {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8"/>
+        <meta charSet="utf-8" />
         <title>Remix: So great, it's funny!</title>
+        <Links />
       </head>
       <body>
         <Outlet></Outlet>
-        {process.env.NODE_ENV === 'development'?(<LiveReload></LiveReload>):null}
+        {process.env.NODE_ENV === 'development' ? <LiveReload></LiveReload> : null}
       </body>
     </html>
   );
